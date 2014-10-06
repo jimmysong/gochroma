2d4b9b8f5365def4ceec78cae601f1ed2bad3b68
Jimmy Song <jaejoon@gmail.com>
2014-10-06 18:59:05 -0500

    Added more methods to blockchain.go
    Restructured blockchain a little bit to help with common methods

--- blockchain.go
+++ blockchain.go
@@ -9,42 +9,103 @@ import (
 	"github.com/conformal/btcwire"
 )
 
-// BlockExplorer represents a place where we can get blockchain data
-// and publish blockchain data
-type BlockExplorer interface {
+// BlockReaderWriter is any place where we can get raw blockchain data
+// and publish raw blockchain data.
+type BlockReaderWriter interface {
 	// Get the current block height
 	GetBlockCount() (int64, error)
 	// Get the hash of the block with index
 	GetBlockHash(index int64) ([]byte, error)
 	// Get the actual block with a given hash
-	GetBlock(hash []byte) (*btcutil.Block, error)
+	GetRawBlock(hash []byte) ([]byte, error)
 	// Get the raw transaction given a hash
-	GetRawTransaction(hash []byte) ([]byte, error)
+	GetRawTx(hash []byte) ([]byte, error)
 	// Publish a raw transaction
-	SendRawTransaction(rawTx []byte) ([]byte, error)
+	SendRawTx(rawTx []byte) ([]byte, error)
 }
 
-type BtcdBlockExplorer struct {
+// BlockExplorer is a struct with methods that return btcutil-style objects
+// from the BlockReaderWriter.
+type BlockExplorer struct {
+	BlockReaderWriter
+}
+
+// GetRawBlockAtHeight returns a byte slice representing the raw blocks
+// that get passed around the blockchain given a height.
+func (b *BlockExplorer) GetRawBlockAtHeight(height int64) ([]byte, error) {
+	hash, err := b.GetBlockHash(height)
+	if err != nil {
+		return nil, err
+	}
+	return b.GetRawBlock(hash)
+}
+
+// GetBlockAtHeight returns a *btcutil.Block struct given a height.
+func (b *BlockExplorer) GetBlockAtHeight(height int64) (*btcutil.Block, error) {
+	raw, err := b.GetRawBlockAtHeight(height)
+	if err != nil {
+		return nil, err
+	}
+	return btcutil.NewBlockFromBytes(raw)
+}
+
+// GetBlock returns a *btcutil.Block struct given a byte-slice hash.
+func (b *BlockExplorer) GetBlock(hash []byte) (*btcutil.Block, error) {
+	raw, err := b.GetRawBlock(hash)
+	if err != nil {
+		return nil, err
+	}
+	return btcutil.NewBlockFromBytes(raw)
+}
+
+// GetPreviousBlock returns the *btcutil.Block struct of the block before
+// a given block identified by the byte-slice hash.
+func (b *BlockExplorer) GetPreviousBlock(hash []byte) (*btcutil.Block, error) {
+	block, err := b.GetBlock(hash)
+	if err != nil {
+		return nil, err
+	}
+	previousHash := block.MsgBlock().Header.PrevBlock
+	return b.GetBlock(previousHash.Bytes())
+}
+
+// GetTx returns the *btcutil.Tx struct of the transaction identified by
+// the byte-slice hash.
+func (b *BlockExplorer) GetTx(hash []byte) (*btcutil.Tx, error) {
+	raw, err := b.GetRawTx(hash)
+	if err != nil {
+		return nil, err
+	}
+	return btcutil.NewTxFromBytes(raw)
+}
+
+// btcdBlockReaderWriter is a specific BlockReaderWriter that uses btcd in order
+// to get all the blockchain data.
+type btcdBlockReaderWriter struct {
 	Net    *btcnet.Params
 	Client *btcrpcclient.Client
 }
 
-func NewBtcdBlockExplorer(net *btcnet.Params, connConfig *btcrpcclient.ConnConfig) (*BtcdBlockExplorer, error) {
+// NewBtcdBlockExplorer returns a BlockExplorer given a network (mainnet/
+// testnet/simnet) and a connection configuration to the btcd instance.
+func NewBtcdBlockExplorer(net *btcnet.Params, connConfig *btcrpcclient.ConnConfig) (*BlockExplorer, error) {
 	client, err := btcrpcclient.New(connConfig, nil)
 	if err != nil {
 		return nil, err
 	}
-	return &BtcdBlockExplorer{
+	return &BlockExplorer{&btcdBlockReaderWriter{
 		Net:    net,
 		Client: client,
-	}, nil
+	}}, nil
 }
 
-func (b *BtcdBlockExplorer) GetBlockCount() (int64, error) {
+// GetBlockCount returns the height of the newest block.
+func (b *btcdBlockReaderWriter) GetBlockCount() (int64, error) {
 	return b.Client.GetBlockCount()
 }
 
-func (b *BtcdBlockExplorer) GetBlockHash(height int64) ([]byte, error) {
+// GetBlockHash returns the byte-slice hash of the block at height given.
+func (b *btcdBlockReaderWriter) GetBlockHash(height int64) ([]byte, error) {
 	hash, err := b.Client.GetBlockHash(height)
 	if err != nil {
 		return nil, err
@@ -52,15 +113,25 @@ func (b *BtcdBlockExplorer) GetBlockHash(height int64) ([]byte, error) {
 	return hash.Bytes(), nil
 }
 
-func (b *BtcdBlockExplorer) GetBlock(hash []byte) (*btcutil.Block, error) {
+// GetRawBlock returns the raw byte-slice of the block identified by the
+// byte-slice hash.
+func (b *btcdBlockReaderWriter) GetRawBlock(hash []byte) ([]byte, error) {
 	shaHash, err := btcwire.NewShaHash(hash)
 	if err != nil {
 		return nil, err
 	}
-	return b.Client.GetBlock(shaHash)
+
+	block, err := b.Client.GetBlock(shaHash)
+	if err != nil {
+		return nil, err
+	}
+
+	return block.Bytes()
 }
 
-func (b *BtcdBlockExplorer) GetRawTransaction(hash []byte) ([]byte, error) {
+// GetRawTx returns the raw byte-slice of the hash identified by the
+// byte-slice hash.
+func (b *btcdBlockReaderWriter) GetRawTx(hash []byte) ([]byte, error) {
 	shaHash, err := btcwire.NewShaHash(hash)
 	if err != nil {
 		return nil, err
@@ -78,7 +149,9 @@ func (b *BtcdBlockExplorer) GetRawTransaction(hash []byte) ([]byte, error) {
 	return ret.Bytes(), nil
 }
 
-func (b *BtcdBlockExplorer) SendRawTransaction(hash []byte) ([]byte, error) {
+// SendRawTx sends the transaction to the blockchain and returns the byte-slice
+// transaction id/hash.
+func (b *btcdBlockReaderWriter) SendRawTx(hash []byte) ([]byte, error) {
 	tx, err := btcutil.NewTxFromBytes(hash)
 	if err != nil {
 		return nil, err
