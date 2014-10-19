3af1db98edd27b8a73c018463defd9d2c7b3b906
Vincenzo Di Somma <vincenzo.disomma@gmail.com>
2014-10-18 20:14:11 -0500

    Promoting MakeError public func.

--- btcd.go
+++ btcd.go
@@ -24,7 +24,7 @@ func NewBtcdBlockExplorer(net *btcnet.Params, connConfig *btcrpcclient.ConnConfi
 	client, err := btcrpcclient.New(connConfig, nil)
 	if err != nil {
 		str := fmt.Sprintf("failed to connect with params, %v", connConfig)
-		return nil, makeError(ErrConnect, str, err)
+		return nil, MakeError(ErrConnect, str, err)
 	}
 	return &BlockExplorer{&btcdBlockReaderWriter{
 		Net:    net,
@@ -42,7 +42,7 @@ func (b *btcdBlockReaderWriter) BlockHash(height int64) ([]byte, error) {
 	hash, err := b.Client.GetBlockHash(height)
 	if err != nil {
 		str := fmt.Sprintf("failed to read at height %d", height)
-		return nil, makeError(ErrBlockRead, str, err)
+		return nil, MakeError(ErrBlockRead, str, err)
 	}
 	return hash.Bytes(), nil
 }
@@ -53,13 +53,13 @@ func (b *btcdBlockReaderWriter) RawBlock(hash []byte) ([]byte, error) {
 	shaHash, err := btcwire.NewShaHash(hash)
 	if err != nil {
 		str := fmt.Sprintf("hash %x looks bad", hash)
-		return nil, makeError(ErrInvalidHash, str, err)
+		return nil, MakeError(ErrInvalidHash, str, err)
 	}
 
 	block, err := b.Client.GetBlock(shaHash)
 	if err != nil {
 		str := fmt.Sprintf("failed to get block %x", hash)
-		return nil, makeError(ErrBlockRead, str, err)
+		return nil, MakeError(ErrBlockRead, str, err)
 	}
 
 	return block.Bytes()
@@ -71,12 +71,12 @@ func (b *btcdBlockReaderWriter) RawTx(hash []byte) ([]byte, error) {
 	shaHash, err := btcwire.NewShaHash(hash)
 	if err != nil {
 		str := fmt.Sprintf("hash %x looks bad", hash)
-		return nil, makeError(ErrInvalidHash, str, err)
+		return nil, MakeError(ErrInvalidHash, str, err)
 	}
 	tx, err := b.Client.GetRawTransaction(shaHash)
 	if err != nil {
 		str := fmt.Sprintf("failed to get tx %x", hash)
-		return nil, makeError(ErrBlockRead, str, err)
+		return nil, MakeError(ErrBlockRead, str, err)
 	}
 	msgTx := tx.MsgTx()
 	var ret bytes.Buffer
@@ -93,12 +93,12 @@ func (b *btcdBlockReaderWriter) TxBlockHash(txHash []byte) ([]byte, error) {
 	shaHash, err := btcwire.NewShaHash(txHash)
 	if err != nil {
 		str := fmt.Sprintf("hash %x looks bad", txHash)
-		return nil, makeError(ErrInvalidHash, str, err)
+		return nil, MakeError(ErrInvalidHash, str, err)
 	}
 	txRawResult, err := b.Client.GetRawTransactionVerbose(shaHash)
 	if err != nil {
 		str := fmt.Sprintf("failed to get tx verbose %x", txHash)
-		return nil, makeError(ErrBlockRead, str, err)
+		return nil, MakeError(ErrBlockRead, str, err)
 	}
 	return hex.DecodeString(txRawResult.BlockHash)
 }
@@ -108,7 +108,7 @@ func (b *btcdBlockReaderWriter) MempoolTxs() ([][]byte, error) {
 	txs, err := b.Client.GetRawMempool()
 	if err != nil {
 		str := fmt.Sprintf("failed to get mempool txs")
-		return nil, makeError(ErrBlockRead, str, err)
+		return nil, MakeError(ErrBlockRead, str, err)
 	}
 	ret := make([][]byte, len(txs))
 	for i, shaHash := range txs {
@@ -123,12 +123,12 @@ func (b *btcdBlockReaderWriter) PublishRawTx(hash []byte) ([]byte, error) {
 	tx, err := btcutil.NewTxFromBytes(hash)
 	if err != nil {
 		str := fmt.Sprintf("failed to convert to tx %x", hash)
-		return nil, makeError(ErrInvalidTx, str, err)
+		return nil, MakeError(ErrInvalidTx, str, err)
 	}
 	shaHash, err := b.Client.SendRawTransaction(tx.MsgTx(), true)
 	if err != nil {
 		str := fmt.Sprintf("failed to publish tx %x", hash)
-		return nil, makeError(ErrBlockWrite, str, err)
+		return nil, MakeError(ErrBlockWrite, str, err)
 	}
 	return shaHash.Bytes(), nil
 }
--- internal_test.go
+++
@@ -1,5 +0,0 @@
-package gochroma
-
-func MakeError(c ErrorCode, d string, e error) ChromaError {
-	return makeError(c, d, e)
-}
